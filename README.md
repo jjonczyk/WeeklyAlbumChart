818e375a901ad5a469a2441381c28b46100e11ef
jjonczyk <51076590+jjonczyk@users.noreply.github.com>
2020-03-07 11:44:03 +0100

    Squash commit
    Short info and instructions are included in the file.
    
    Update README.md
    
    tiny changes
    
    tiny changes
    
    tiny changes
    
    tiny changes
    
    .gitignore added
    
    update
    
    update
    
    API key errors fixed
    
    Cleaning commit
    
    Cleaning commit
    
    Cleaning commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Author:             Jakub Jonczyk  
+Project made:       7.11.2019  
+Repository created: 7.03.2020  
+---------------------------------
+I like statistics, programming and music so I've made this simple app just for fun.  
+
+Requirements:  
+You need to have an account on last.fm.  
+
+About my code:  
+If you have an account already, you'll probably know that last.fm can collect ('scrobble')  
+some data from streaming services like Spotify, Tidal, etc...  
+As you run this code, all you need to do is type your username  
+and the app will print some basic statistics based on your favourite music from last week (using API).  
+
+Enjoy!
--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 from urllib.request import urlopen
 import json
 
+import pdb
+
 def print_author():
     info = '''
-    Author: Jakub Jonczyk
-    Date:   7.11.2019
+    Author:     Jakub Jonczyk
+    Created at: 7.11.2019
     '''
     print(info)
+
+
+def enter_key():
+    key = input('Enter your last.fm API key: ')
+    print(key)
+    return key
+
 
 #get last.fm API key
 def get_key():
     file = open("lastfm_key.txt")
     key = file.readline()
     file.close()
-    return key
+    if key:
+        return key
+    else:
+        key = enter_key()
+        return key
 
 def get_user():
     user = input("type Your last.fm username: ")
     return user
 
+
 def weekly_album_chart(user, key, n):
-    user = username
-    key = get_key()
     with urlopen(f"http://ws.audioscrobbler.com/2.0?method=user.getweeklyalbumchart&user={user}&api_key={key}&format=json") as file:
         try:
             text = file.read().decode("utf-8")
@@ -34,13 +46,19 @@ def weekly_album_chart(user, key, n):
         except:
             raise ConnectionError
 
-def print_wait_screen():
-    print(f"\nI've just asked last.fm for {username}'s favourite albums! \nAPI key: {get_key()}\n")
+def print_wait_screen(user, key):
+    print(f"\nI've just asked last.fm for {user}'s favourite albums! \nAPI key: {key}\n")
     print("   Your weekly top scrobbled are:")
 
+
 username = get_user()
-counter = 0;
-print_wait_screen()
-for x in range(10):
-    weekly_album_chart(username, get_key(), x)
+my_key = get_key()
+print_wait_screen(username, my_key)
+
+try:
+    for x in range(10):
+        weekly_album_chart(username, my_key, x)
+except:
+    print("Sorry, your data is invalid :(")
+
 print_author()
