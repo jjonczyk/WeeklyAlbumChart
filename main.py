2b4706a0c2e54dce77a87ddd516de360c854bd7d
Jakub Jonczyk <j.jonczyk.1@stud.elka.pw.edu.pl>
2020-10-19 18:23:18 +0200

    Cleaning up the code...

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 import json
 
-import pdb
+USER = 'user'
+KEY = 'key'
+API_ACCOUNT_URL = "https://www.last.fm/api/account/create"
+PATHS = {
+    'key': "lastfm_key.txt",
+    'user': "username.txt"
+}
+
 
 def print_author():
     info = '''
@@ -11,54 +19,58 @@ def print_author():
     print(info)
 
 
-def enter_key():
-    key = input('Enter your last.fm API key: ')
-    print(key)
-    return key
+def type_data(what: str):
+    data = None
+    while not data:
+        data = input(f"It seems you don't have your {what} saved in {PATHS[what]} file. \n"
+                     "Please, type it now: ")
+    print("Thanks!")
+    return data
 
 
-#get last.fm API key
-def get_key():
-    file = open("lastfm_key.txt")
-    key = file.readline()
-    file.close()
+def get_data(what: str):
+    try:
+        with open(PATHS[what]) as file:
+            key = file.readline()
+    except FileNotFoundError:
+        print(f"{what} file not found")
+        key = type_data(what)
     if key:
         return key
     else:
-        key = enter_key()
-        return key
-
-def get_user():
-    user = input("type Your last.fm username: ")
-    return user
+        print(f"Please, enter the {what} properly")
+        get_data(what)
 
 
 def weekly_album_chart(user, key, n):
     with urlopen(f"http://ws.audioscrobbler.com/2.0?method=user.getweeklyalbumchart&user={user}&api_key={key}&format=json") as file:
-        try:
-            text = file.read().decode("utf-8")
-            object = json.loads(text)["weeklyalbumchart"]["album"]
-            rank =  object[n]["@attr"]["rank"]
-            artist = object[n]["artist"]["#text"]
-            album = object[n]["name"]
-            playcount = object[n]["playcount"]
-            print(f"{rank}: {album} by {artist} | played {playcount} times")
-        except:
-            raise ConnectionError
+        text = file.read().decode("utf-8")
+        obj = json.loads(text)["weeklyalbumchart"]["album"]
+        rank = obj[n]["@attr"]["rank"]
+        artist = obj[n]["artist"]["#text"]
+        album = obj[n]["name"]
+        playcount = obj[n]["playcount"]
+        print(f"{rank}: {album} by {artist} | played {playcount} times")
 
-def print_wait_screen(user, key):
-    print(f"\nI've just asked last.fm for {user}'s favourite albums! \nAPI key: {key}\n")
+
+def print_wait_screen(user):
+    print(f"\nI've just asked last.fm for {user}'s favourite albums!")
     print("   Your weekly top scrobbled are:")
 
+# --------------------------------------------
+# ---------------- LAUNCH --------------------
+# --------------------------------------------
 
-username = get_user()
-my_key = get_key()
-print_wait_screen(username, my_key)
+
+username = get_data(USER)
+my_key = get_data(KEY)
+print_wait_screen(username)
 
 try:
     for x in range(10):
         weekly_album_chart(username, my_key, x)
-except:
-    print("Sorry, your data is invalid :(")
+except HTTPError:
+    print("Your data seems to be invalid - sorry, I have to close the app :(\n"
+          f"If you don't have your last.fm API key, visit: {API_ACCOUNT_URL}")
 
 print_author()
